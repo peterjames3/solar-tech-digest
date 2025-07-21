441f4bafa3121da75ac1d27fc058d6a4de7b2158
peterjames3 <kamauchief97@gmail.com>
2025-07-21 17:37:47 +0300

    setting up dynamic path for blog posts

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,14 +3,14 @@ export const metadata = {
 };
 import Hero from "@/app/NativeComponents/Hero";
 import FeaturedCompany from "@/app/NativeComponents/FeaturedCompany";
-import FeaturedInsights from "@/app/NativeComponents/FeaturedInsights";
+
 
 export default function Home() {
   return (
     <main className="my-32">
       <Hero />
       <FeaturedCompany />
-      <FeaturedInsights />
+     
     </main>
   );
 }
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,16 +1,21 @@
 import { groq } from "next-sanity";
 
 // Get all posts
+//Get all posts
 
-export const allPosts = groq`*[_type == 'posts']{
-_createdAt,
-_id,
-title,
-slug,
-mainImage,
-"imageURL": mainImage.asset ->url,
-"authorName":author -.name,}`;
-
+export const postsQuery = groq`
+*[_type == "post"]{
+   _id,
+   _createdAt,
+   description,
+    title,
+   slug,
+   mainImage,
+  "imageURL": mainImage.asset->url,
+   "authorName": author->name,
+   "categories": categories[]->{title, description},
+ }
+ `;
 // Get a single post by its slug
 export const postQuery = groq`*[_type == "posts" && slug.current == $slug][0]{ 
     title, description, mainImage, body
@@ -21,9 +26,9 @@ export const postPathsQuery = groq`*[_type == "posts" && defined(slug.current)][
     "params": { "slug": slug.current }
   }`;
 
-  //get all post including their categories
+//get all post including their categories
 
-  export const allSolarPosts = groq`*[_type == "posts"] | order(_createdAt, desc){
+export const allSolarPosts = groq`*[_type == "posts"] | order(_createdAt, desc){
    _id,
    _createdAt,
    title,
@@ -32,12 +37,12 @@ export const postPathsQuery = groq`*[_type == "posts" && defined(slug.current)][
    mainImage,
    "imageUrl": mainImage.asset->url,
    "authorName": author->name,
-  }`
+  }`;
 
-  //Fetching posts based on their categories
-  // category: Off-Grid & Backup Power: Solutions for Uninterrupted Energy
+//Fetching posts based on their categories
+// category: Off-Grid & Backup Power: Solutions for Uninterrupted Energy
 
-  export const offGridBackupPowerPosts = groq`*[_type == 'post' && 'Off-Grid & Backup Power: Solutions for Uninterrupted Energy' in categories[]->title] | order(_createdAt, desc){
+export const offGridBackupPowerPosts = groq`*[_type == 'post' && 'Off-Grid & Backup Power: Solutions for Uninterrupted Energy' in categories[]->title] | order(_createdAt, desc){
   _id,
   _createdAt,
   title,
@@ -46,9 +51,9 @@ export const postPathsQuery = groq`*[_type == "posts" && defined(slug.current)][
   mainImage,
   "imageUrl": mainImage.asset->url,
   "authorName": author->name,
-  }`
+  }`;
 //category: Best Solar & battery products
-  export const solarBatteryProductsPosts = groq`*[_type == 'post' && 'Best Solar & Battery Products: Comparisons & Expert Buying Guides' in categories[]->title] | order(_createdAt, desc){
+export const solarBatteryProductsPosts = groq`*[_type == 'post' && 'Best Solar & Battery Products: Comparisons & Expert Buying Guides' in categories[]->title] | order(_createdAt, desc){
   _id,
   _createdAt,
   title,
@@ -58,7 +63,7 @@ export const postPathsQuery = groq`*[_type == "posts" && defined(slug.current)][
   "imageUrl": mainImage.asset->url,
   "authorName":author->name,
 
-  }`
+  }`;
 //category: renewable  energy news
 export const renewableEnergyPosts = groq`*[_type == 'post' && 'Renewable Energy News: Industry Updates, Innovations & Trends' in categories[]->title ] | order(_createdAt, desc){
 _id,
@@ -69,7 +74,7 @@ title,
 mainImage,
 "imageUrl":mainImage.asset->url,
 "authorName":author->name,
-}`
+}`;
 //category: solar installation & maintenance: tips, tricks & Expert advice
 export const solarPanelPosts = groq`*[_type == 'post' && 'Solar Panels: Installation, Reviews, Efficiency Tips & maintenance' in categories[]->title ] | order(_createdAt, desc){
 _id,
@@ -80,7 +85,7 @@ title,
 mainImage,
 "imageUrl":mainImage.asset->url,
 "authorName":author->name,
-}`
+}`;
 //category: Batteries & inverters: Reviews, Maintenance, buting Guides 7 Tips
 export const batteriesInvertersPosts = groq`*[_type == 'post' && 'Batteries & Inverters: Reviews, Maintenance, buying guides & Tips' in categories[]->title ] | order(_createdAt, desc){
 _id,
@@ -91,4 +96,21 @@ title,
 mainImage,
 "imageUrl":mainImage.asset->url,
 "authorName":author->name,
-}`
+}`;
+
+// Get the latest posts by published date, limited to the first 5 posts
+export const latestPostsQuery = groq`
+  *[_type == "post"] | order(_createdAt desc) [0...8] {
+  _id,
+    _createdAt,
+    title,
+    slug,
+    body,
+    mainImage,
+    "imageURL": mainImage.asset->url,
+    "authorName": author->name,
+     "authorImage": author->image.asset->url,
+    "categories": categories[]->{title, description}
+    
+  }
+`;
